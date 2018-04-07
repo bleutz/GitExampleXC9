f5e467fbc39b60f849a18798df273bba91dcf6a8
Brian Leutz <bdleutz@gmail.com>
2018-04-07 13:12:19 -0400

    merged back to master

--- GitExampleXC9/ViewController.swift
+++ GitExampleXC9/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("This is a change from the master branch")
+        print("This is a change from the master branch - now tied to the master branch")
     }
 
     override var representedObject: Any? {
