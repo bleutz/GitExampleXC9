bd3c175c320567054dcd8396debbb7c1db58a1fb
Brian Leutz <bdleutz@gmail.com>
2018-04-07 13:01:13 -0400

    Initial Commit

---
+++ GitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC9
+//
+//  Created by Brian Leutz on 4/7/18.
+//  Copyright © 2018 SymbioMate. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
