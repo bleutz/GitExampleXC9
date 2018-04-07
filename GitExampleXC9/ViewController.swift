324855bf8101f8e0b958fa8f072e47efdf20e05c
Brian Leutz <bdleutz@gmail.com>
2018-04-07 13:09:32 -0400

    Testing a new branch

--- GitExampleXC9/ViewController.swift
+++ GitExampleXC9/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("some change")
+        print("This is a change from the master branch")
     }
 
     override var representedObject: Any? {
